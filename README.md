ed62702021dcb63c19459eb1bca233e447a27171
Supriya Sharma <sharmasup565@gmail.com>
2022-04-26 02:24:12 -0700

    write Employee class tests, constructors, and methods

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. Create initial file structure.
 1. Initialize npm and install dependencies.
+1. Write tests for object classes.
---
+++ _tests_/Employee.test.js
@@ -0,0 +1,66 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should create an object with a name, id, and email when provided valid arguments", () => {
+            const name = "Kevin";
+            const id = 104;
+            const email = "kevin@company.com";
+            const employee = new Employee(name, id, email);
+            expect(employee.name).toEqual(name);
+            expect(employee.id).toEqual(id);
+            expect(employee.email).toEqual(email);
+        });
+
+        it("should throw an error if no arguments are provided", () => {
+            const cb = () => new Employee();
+            expect(cb).toThrow();
+        });
+
+        it("should throw an error if id is not provided", () => {
+            const name = "Kevin";
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+            const cb = () => new Employee(name);
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if 'id' is less than 0", () => {
+            const name = "Kevin";
+            const id = -77;
+            const email = "kevin@company.com";
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+            const cb = () => new Employee(name, id, email);
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if email is not provided", () => {
+            const name = "Kevin";
+            const id = 104;
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+            const cb = () => new Employee(name, id);
+            expect(cb).toThrowError(err);
+        });
+
+        // it("should throw an error if name is not a string");
+        // it("should throw an error if id is not a number");
+    });
+
+    describe("Getters", () => {
+        it("should return the employee name when getName() is called", () => {
+            const employee = new Employee("Rebecca", 17, "rebecca@company.com");
+            expect(employee.getName()).toEqual("Rebecca");
+        });
+        it("should return the employee id when getId() is called", () => {
+            const employee = new Employee("Rebecca", 17, "rebecca@company.com");
+            expect(employee.getId()).toEqual(17);
+        });
+        it("should return the employee email when getEmail() is called", () => {
+            const employee = new Employee("Rebecca", 17, "rebecca@company.com");
+            expect(employee.getEmail()).toEqual("rebecca@company.com");
+        });
+        it("should return 'Employee' when getRole() is called", () => {
+            const employee = new Employee("Rebecca", 17, "rebecca@company.com");
+            expect(employee.getRole()).toEqual("Employee");
+        });
+    });
+});

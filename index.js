36901eb021231fd16f64baa510a0626d5071585c
Supriya Sharma <sharmasup565@gmail.com>
2022-04-28 22:20:24 -0700

    implement write HTML file

---
+++ index.js
@@ -0,0 +1,158 @@
+// import packages and content generator
+const inquirer = require("inquirer");
+const fs = require("fs");
+const content = require("./src/generateHTML");
+
+// import classes
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const employees = [];
+
+// initialize app, get responses for a manager
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter your manager's name:"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter your manager's employee ID:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter your manager's email:"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Please enter your manager's office number:"
+            },
+            {
+                type: "list",
+                name: "nextEmployee",
+                message: "Would you like to add another member to this team?",
+                choices: ["Engineer", "Intern", "Done"]
+            }
+        ])
+        .then((data) => {
+            try {
+                employees.push(new Manager(data.name, data.id, data.email, data.officeNumber));
+            } catch (err) {
+                console.log("Error creating Manager: " + err);
+            }
+            addNextEmployee(data.nextEmployee);
+        });
+}
+
+// get responses for an engineer
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the engineer's name:"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the engineer's employee ID:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the engineer's email:"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Please enter the engineer's github username:"
+            },
+            {
+                type: "list",
+                name: "nextEmployee",
+                message: "Would you like to add another member to this team?",
+                choices: ["Engineer", "Intern", "Done"]
+            }
+        ])
+        .then((data) => {
+            try {
+                employees.push(new Engineer(data.name, data.id, data.email, data.github));
+            } catch (err) {
+                console.log("Error creating Engineer: " + err);
+            }
+            addNextEmployee(data.nextEmployee);
+        });
+}
+
+// get responses for an intern
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the intern's name:"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the intern's employee ID:"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the intern's email:"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Please enter the intern's school:"
+            },
+            {
+                type: "list",
+                name: "nextEmployee",
+                message: "Would you like to add another member to this team?",
+                choices: ["Engineer", "Intern", "Done"]
+            }
+        ])
+        .then((data) => {
+            try {
+                employees.push(new Intern(data.name, data.id, data.email, data.school));
+            } catch (err) {
+                console.log("Error creating Intern: " + err);
+            }
+            addNextEmployee(data.nextEmployee);
+        });
+}
+
+function addNextEmployee(nextEmployee) {
+    if (nextEmployee === "Engineer") {
+        addEngineer();
+    } else if (nextEmployee === "Intern") {
+        addIntern();
+    } else {
+        writeHTML();
+    }
+}
+
+//var html = content.generateHTML(employees);
+    //fs.writeFile("./dist/index.html", html, (err) =>
+    //    err ? console.log(err) : console.log("success"))
+
+function writeHTML() {
+    var html = content.generateHTML(employees);
+    fs.writeFile("./dist/index.html", html, (err) =>
+        err ? console.log(err): console.log("success")
+    );
+}
+
+// call initialize
+init()
---
+++ src/generateHTML.js
@@ -0,0 +1,68 @@
+// import classes
+const Manager = require("../lib/Manager");
+const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
+
+function generateHTML(employees) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Team Profile</title>
+            <link rel="stylesheet" type="text/css" href="./normalize.css" />
+            <link rel="stylesheet" type="text/css" href="./style.css" />
+        </head>
+        <body>
+    
+            <header class="banner">
+                <h1>My Team</h1>
+            </header>
+    
+            <main>
+
+                <!-- Employee cards -->
+                ${generateCards(employees)}
+
+            </main>
+    
+        </body>
+    </html>`;
+}
+
+function generateCards(employees) {
+    let cardsHTML = ``;
+    for (let i = 0; i < employees.length; i++) {
+        const position = employees[i].getRole();
+        let emoji;
+        let thirdItem;
+        if (position === "Manager") {
+            emoji = `&#128083;`
+            thirdItem = `Office Number: ${employees[i].getOfficeNumber()}`
+        } else if (position === "Engineer") {
+            emoji = `&#9881;`
+            thirdItem = `GitHub: <a href="https://github.com/${employees[i].getGithub()}">${employees[i].getGithub()}</a>`
+        } else {
+            emoji = `&#127891;`
+            thirdItem = `School: ${employees[i].getSchool()}`
+        }
+
+        cardsHTML += `
+        <div class="card">
+            <header>
+                <h2>${employees[i].getName()}</h2>
+                <h3>${emoji} ${position}</h3>
+            </header>
+            <ul>
+            <!-- a href="mailto: sharmasup565@gmail.com">shar -->
+                <li>ID: ${employees[i].getId()}</li>
+                <li>Email: <a href="mailto: ${employees[i].getEmail()}">${employees[i].getEmail()}</a></li>
+                <li>${thirdItem}</li>
+            </ul>
+        </div>
+        `
+    }
+    return cardsHTML;
+}
+
+module.exports = { generateHTML };
